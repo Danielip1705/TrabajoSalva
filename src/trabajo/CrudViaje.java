8889cbd10f9ac0251fb36fb1c76405ee6774983c
Danielip1705 <daniel.iglesias@iesnervion.es>
2024-05-14 08:42:35 +0200

    Creacion de la funcion mostrar(): Daniel Creacion de la funcion anyadirViaje(): Jesus Actualización del sprint 1: Todos

---
+++ src/trabajo/CrudViaje.java
@@ -0,0 +1,19 @@
+package trabajo;
+
+/**
+ * Clase donde se realizaran los metodos necesarios para realizar el crud
+ * C create (añadir viaje)
+ * R read (Mostrar listado)
+ * U update (modificar precio y fecha existente)
+ * D delete (eliminar viaje)
+ */
+public class CrudViaje {
+
+	/*
+	 *Posibles funciones (Creo)
+	 *añadirViaje(Viaje vij)
+	 *listado()
+	 *delete(Viaje vij)
+	 *guardarEnArchivo()
+	 */
+}

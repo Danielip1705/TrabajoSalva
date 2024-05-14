0b726d7449e2071b9ae6a35e9061c21e3bb18b8f
Danielip1705 <daniel.iglesias@iesnervion.es>
2024-05-14 08:45:14 +0200

    Parte Daniel Terminada

--- src/trabajo/CrudViaje.java
+++ src/trabajo/CrudViaje.java
@@ -1,19 +1,24 @@
 package trabajo;
 
+import java.util.ArrayList;
+
 /**
- * Clase donde se realizaran los metodos necesarios para realizar el crud
- * C create (añadir viaje)
- * R read (Mostrar listado)
- * U update (modificar precio y fecha existente)
- * D delete (eliminar viaje)
+ * Clase donde se realizaran los metodos necesarios para realizar el crud C
+ * create (añadir viaje) R read (Mostrar listado) U update (modificar precio y
+ * fecha existente) D delete (eliminar viaje)
  */
 public class CrudViaje {
 
+	static ArrayList<Viaje> listaViajes = new ArrayList<Viaje>();
 	/*
-	 *Posibles funciones (Creo)
-	 *añadirViaje(Viaje vij)
-	 *listado()
-	 *delete(Viaje vij)
-	 *guardarEnArchivo()
+	 * Posibles funciones (Creo) añadirViaje(Viaje vij) listado() delete(Viaje vij)
+	 * guardarEnArchivo()
 	 */
+
+	public static void mostrarViajes() {
+
+		for (Viaje vij : listaViajes) {
+			System.out.println(vij);
+		}
+	}
 }
